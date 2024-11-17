bb6810e698c1c3f3d8078ac423739de544ee2307
Mistralys <s.mordziol@gmail.com>
2024-11-17 11:51:13 +0100

    Added error collecting.

--- src/MarkdownRenderer/Processors/BaseProcessor.php
+++ src/MarkdownRenderer/Processors/BaseProcessor.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Mistralys\MarkdownRenderer\Processors;
 
+use AppUtils\OperationResult_Collection;
 use AppUtils\Traits\OptionableTrait;
 use Mistralys\MarkdownRenderer\Renderer;
 
@@ -12,14 +13,31 @@ abstract class BaseProcessor implements ProcessorInterface
     use OptionableTrait;
 
     protected Renderer $renderer;
+    private OperationResult_Collection $validationResults;
 
     public function __construct(Renderer $renderer)
     {
         $this->renderer = $renderer;
+        $this->validationResults = new OperationResult_Collection($this);
     }
 
     public function getRenderer(): Renderer
     {
         return $this->renderer;
+    }
+
+    protected function addError(string $message, int $code) : void
+    {
+        $this->validationResults->makeError($message, $code);
+    }
+
+    protected function getNextPlaceholder() : string
+    {
+        return $this->renderer->getNextPlaceholder();
+    }
+
+    public function getResults() : OperationResult_Collection
+    {
+        return $this->validationResults;
     }
 }
