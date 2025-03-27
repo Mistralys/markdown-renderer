0ef1de6336f17df865e9bea3eaf57d5e347666c0
Mistralys <s.mordziol@gmail.com>
2025-03-27 13:26:15 +0100

    Added tests.

--- tests/MarkdownRenderer/Tests/ImageProcessorTests.php
+++ tests/MarkdownRenderer/Tests/ImageProcessorTests.php
@@ -12,15 +12,40 @@ final class ImageProcessorTests extends RendererTestCase
 {
     public function test_allAttributes() : void
     {
-        $renderer = Renderer::factory('{image: "test.jpg" width="45px" property class="className" title="Argh! \"Subquote\" here" "noname" property}');
+        $renderer = Renderer::factory('{image: "test.jpg" width="45px" height="33px" property class="className" alt="Alt text" title="Argh! \"Subquote\" here" id="imageID" "noname" property}');
         $renderer->addProcessor(new ImagesProcessor($renderer));
 
         $html = $renderer->render();
 
         $this->assertStringContainsString('src="/test.jpg"', $html);
-        $this->assertStringContainsString('style="width:45px"', $html);
+        $this->assertStringContainsString('style="height:33px;width:45px"', $html);
         $this->assertStringContainsString('class="className"', $html);
         $this->assertStringContainsString('title="Argh! &quot;Subquote&quot; here"', $html);
+        $this->assertStringContainsString('alt="Alt text"', $html);
+        $this->assertStringContainsString('id="imageID"', $html);
+    }
+
+    public function test_freeSpacing() : void
+    {
+        $image = <<<'MARKDOWN'
+{image: 
+    "test.jpg" 
+    width="42px" 
+    class="className"
+    title="Title" 
+    id="imageID"
+}
+MARKDOWN;
+
+        $renderer = Renderer::factory($image);
+        $renderer->addProcessor(new ImagesProcessor($renderer));
+
+        $html = $renderer->render();
+
+        $this->assertEquals(
+            '<p><img src="/test.jpg" id="imageID" title="Title" alt="Title" class="className" style="width:42px"/></p>',
+            trim($html)
+        );
     }
 
     public function test_missingFile() : void
--- tests/MarkdownRenderer/Tests/SiteURLTests.php
+++ tests/MarkdownRenderer/Tests/SiteURLTests.php
@@ -10,25 +10,7 @@ use Mistralys\MarkdownRenderer\Renderer;
 
 final class SiteURLTests extends RendererTestCase
 {
-    private const TEST_LINK_DEFS = <<<MD
-[LinkDef1][]
-[LinkDef2][]
-[LinkDef3][]
-[LinkDef4][]
-
-[LinkDef1]: siteurl?param1=value1
-[LinkDef2]: siteurl/Path/To/Page
-[LinkDef3]: siteurl/Path/To/Page?param1=value1&param2=value2
-[LinkDef4]: siteurl
-MD;
-
-    private const TEST_INLINE_LINKS = <<<MD
-[Link1](siteurl?param1=value1)
-[Link2](siteurl/Path/To/Page)
-[Link3](siteurl/Path/To/Page?param1=value1&param2=value2)
-[Link4](siteurl)
-MD;
-
+    // region: _Tests
 
     public function test_linkDefs() : void
     {
@@ -70,4 +52,67 @@ MD;
         $this->assertStringContainsString('href="https://127.0.0.1/webroot/Path/To/Page/?param1=value1&amp;param2=value2"', $html);
         $this->assertStringContainsString('href="https://127.0.0.1/webroot/"', $html);
     }
+
+    public function test_paramsCallback() : void
+    {
+        $renderer = Renderer::factory('[Link1](siteurl?param1=value1)');
+        $renderer->addProcessor(
+            (new SiteURLProcessor($renderer))
+                ->setSiteURL('https://127.0.0.1/webroot')
+                ->setParamsCallback($this->adjustParams(...))
+        );
+
+        $html = $renderer->render();
+
+        $this->assertTrue($this->paramsCallbackCalled);
+
+        $this->assertStringContainsString('href="https://127.0.0.1/webroot/?foo=bar&amp;param1=value1"', $html);
+    }
+
+    /**
+     * @param array<string,string> $params
+     * @return array<string,string>
+     */
+    private function adjustParams(array $params) : array
+    {
+        $params['foo'] = 'bar';
+
+        $this->paramsCallbackCalled = true;
+
+        return $params;
+    }
+
+    // endregion
+
+    // region: Support methods
+
+    private const TEST_LINK_DEFS = <<<MD
+[LinkDef1][]
+[LinkDef2][]
+[LinkDef3][]
+[LinkDef4][]
+
+[LinkDef1]: siteurl?param1=value1
+[LinkDef2]: siteurl/Path/To/Page
+[LinkDef3]: siteurl/Path/To/Page?param1=value1&param2=value2
+[LinkDef4]: siteurl
+MD;
+
+    private const TEST_INLINE_LINKS = <<<MD
+[Link1](siteurl?param1=value1)
+[Link2](siteurl/Path/To/Page)
+[Link3](siteurl/Path/To/Page?param1=value1&param2=value2)
+[Link4](siteurl)
+MD;
+
+    private bool $paramsCallbackCalled = false;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->paramsCallbackCalled = false;
+    }
+
+    // endregion
 }
